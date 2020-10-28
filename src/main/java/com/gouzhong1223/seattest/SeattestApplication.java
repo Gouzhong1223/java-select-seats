58280230fc035315d0ac0a09c65e55b2a94ccbaf
Gouzhong1223 <1162864960@qq.com>
2020-10-28 11:15:41 +0800

    添加相关依赖和相关配置

--- src/main/java/com/gouzhong1223/seattest/SeattestApplication.java
+++ src/main/java/com/gouzhong1223/seattest/SeattestApplication.java
@@ -3,11 +3,16 @@ package com.gouzhong1223.seattest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author qingsong
+ */
 @SpringBootApplication
 public class SeattestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SeattestApplication.class, args);
     }
+
+
 
 }
