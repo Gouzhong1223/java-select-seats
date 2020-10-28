ca4db1374736bd89d773a902e2d5f492e8e4d03e
Gouzhong1223 <1162864960@qq.com>
2020-10-28 10:48:37 +0800

    init

---
+++ src/main/java/com/gouzhong1223/seattest/SeattestApplication.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.seattest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeattestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeattestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gouzhong1223/seattest/SeattestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.seattest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeattestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
